be535108fb07972b2b923bde5785a1b59d1f6929
Marc CARRE <carre.marc@gmail.com>
2015-08-02 14:50:10 +0100

    SRM 664 - DIV 2 - 250 points - BearCheats: Added system tests.

--- src/test/java/singleroundmatch664/BearCheatsTest.java
+++ src/test/java/singleroundmatch664/BearCheatsTest.java
@@ -65,4 +65,25 @@ public class BearCheatsTest {
 		assertThat(__result, is(__expected));
 	}
 
+	@Test
+	public void systemTests() {
+		assertThat(new BearCheats().eyesight(385900, 123000), is("glasses"));
+		assertThat(new BearCheats().eyesight(1, 1), is("happy"));
+		assertThat(new BearCheats().eyesight(99, 99), is("happy"));
+		assertThat(new BearCheats().eyesight(98, 89), is("glasses"));
+		assertThat(new BearCheats().eyesight(222, 323), is("glasses"));
+		assertThat(new BearCheats().eyesight(654321, 123456), is("glasses"));
+		assertThat(new BearCheats().eyesight(848097, 848997), is("happy"));
+		assertThat(new BearCheats().eyesight(50000, 10000), is("happy"));
+		assertThat(new BearCheats().eyesight(1111, 1011), is("happy"));
+		assertThat(new BearCheats().eyesight(999758, 999758), is("happy"));
+		assertThat(new BearCheats().eyesight(879000, 889000), is("happy"));
+		assertThat(new BearCheats().eyesight(59390, 58391), is("glasses"));
+		assertThat(new BearCheats().eyesight(278548, 276348), is("glasses"));
+		assertThat(new BearCheats().eyesight(109, 110), is("glasses"));
+		assertThat(new BearCheats().eyesight(1000, 1089), is("glasses"));
+		assertThat(new BearCheats().eyesight(12, 22), is("happy"));
+		assertThat(new BearCheats().eyesight(12121, 22222), is("glasses"));
+		assertThat(new BearCheats().eyesight(11, 23), is("glasses"));
+	}
 }
